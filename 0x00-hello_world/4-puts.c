4c0a972a20c27d802efb9798e9862c98b0a0565d
2021BTech <daffydanni@gmail.com>
2022-10-13 08:31:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * REturn: Always 0
+ * main - Entry point
+ * Description: prints "\"Proramming is like building a multilingual puzzel"
+ * Return: 0
  */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
