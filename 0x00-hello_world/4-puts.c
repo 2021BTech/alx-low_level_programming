8395048b7d884161c301df0afa7bd2bbcdfdf811
2021BTech <daffydanni@gmail.com>
2022-10-13 08:57:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: prints "\"Proramming is like building a multilingual puzzel"
- * Return: 0
+ * main- Serves as the entry point for the program
+ * Return: By default returns zero if no erros otherwise returns non-zero value.
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
