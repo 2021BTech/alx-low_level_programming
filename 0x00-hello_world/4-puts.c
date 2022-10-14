a038f1915ec169eface16aadb341a356a42a22a7
2021BTech <daffydanni@gmail.com>
2022-10-13 18:10:03 -0700

    Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);;
+
+		return (0);
+};
