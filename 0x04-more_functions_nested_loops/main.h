6c6c621def5dcc453389befe12108340cfb15eb8
2021BTech <daffydanni@gmail.com>
2022-10-20 07:05:45 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -12,8 +8,10 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int isPrime(long int n);
-long int biggestFactor(long int a);
 void print_number(int n);
-int numDigits(int n);
-#endif
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */
