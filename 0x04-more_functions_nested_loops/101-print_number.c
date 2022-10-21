7698bd1da42356fab79752ddb24d6f00f73fd16b
2021BTech <daffydanni@gmail.com>
2022-10-20 19:00:48 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,43 @@
  */
 void print_number(int n)
 {
-	unsigned int x;
+	int i, j, digit, digits, power;
+	unsigned int temp, numchar, number;
 
-	x = n;
+	digit = 0;
+
 	if (n < 0)
 	{
-		_putchar(45);
-		x = -n;
+		_putchar('-');
+		temp = -n;
 	}
-	if (x / 10)
+	else
 	{
-		print_number(x / 10);
-		_putchar((x % 10) + '0');
+		temp = n;
+	}
+
+	number = temp;
+
+	while (number >= 10)
+	{
+		number = number / 10;
+		digit++;
+	}
+	digits = digit + 1;
+	power = 1;
+	i = 1;
+
+	while (i < digits)
+	{
+		power = power * 10;
+		i++;
+	}
+	j = power;
+	while (j >= 1)
+	{
+		numchar = (temp / j) % 10;
+		_putchar(numchar + '0');
+		j = j / 10;
 	}
 }
+
