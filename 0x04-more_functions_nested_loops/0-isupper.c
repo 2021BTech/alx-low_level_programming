1b8c9b1373631c18dd22c6d1ea31d5885263b4d6
2021BTech <daffydanni@gmail.com>
2022-10-20 17:23:55 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
- * _isupper - function that verifies if a  character is uppercase or not
- * @c: tested character
- * Return: returns 1 if it is uppercase , 0 if not
+ * _isupper - checks if parameter is an uppercase character.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
  */
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
-
+	}
+	else
+	{
 		return (0);
-}
+	}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -8,10 +11,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
-int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
+int _putchar(char c);
 
-#endif /* MAIN_H */
+#endif
