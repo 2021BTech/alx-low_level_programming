79691c94ef486b519db81bafdb174330e42c543a
2021BTech <daffydanni@gmail.com>
2022-10-24 03:18:19 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -29,36 +29,3 @@ void rev_string(char *s)
 
 	end[len + 1] = '\0';
 }
-/**
- * _strlen - returns the length of a string
- * @s: string
- * Return: returns lenght;
- */
-int _strlen(char *s)
-{
-	int count, inc;
-
-	inc = 0;
-
-	for (count = 0; s[count] != '\0'; count++)
-	{
-		inc++;
-	}
-
-	return (inc);
-}
-
-/**
- * swap_char - swap two characters
- * @a: first character
- * @b: second character
- * Return: nothing
- */
-
-void swap_char(char *a, char *b)
-{
-	char tmp = *a;
-
-	*a = *b;
-	*b = tmp
-}
