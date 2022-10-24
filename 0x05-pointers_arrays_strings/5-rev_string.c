0c36460bc753050f239e2ac27fb842d09443f5ba
2021BTech <daffydanni@gmail.com>
2022-10-24 02:53:55 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -23,8 +23,7 @@ void rev_string(char *s)
  * Return: returns lenght;
  */
 int _strlen(char *s)int _strlen(char *s)
-{
-	int count, inc;
+{	int count, inc;
 
 	inc = 0;
 	for (count = 0; s[count] != '\0'; count++)
