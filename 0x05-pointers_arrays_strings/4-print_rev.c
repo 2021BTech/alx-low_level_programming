618aa8d94cd7b6d5e46350cfa6cf057ddce935fe
2021BTech <daffydanni@gmail.com>
2022-10-24 02:25:45 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,15 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index = 0;
+	int index;
 
-	len = _strlen(s);
-	for (index = len - 1; index >= 0; index--)
+	for (index = 0; s[index] != '\0'; index++)
 	{
-		_putchar(s[index]);
-	}
+		for (index = index - 1; s[index] != '\0'; index--)
+		{
+			_putchar(s[index]);
+		}
 
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
