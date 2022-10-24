9623a2a2c6a3a174e091dba6dd517dcc619cc34f
2021BTech <daffydanni@gmail.com>
2022-10-24 02:22:28 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	int len = 0, index = 0;
+
+	len = _strlen(s);
+	for (index = len - 1; index >= 0; index--)
+	{
+		_putchar(s[index]);
+	}
+
+	_putchar('\n');
+}
