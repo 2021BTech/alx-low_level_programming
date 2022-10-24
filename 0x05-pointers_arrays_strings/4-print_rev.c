06a215e3b83d553869bb7fa10afc9294b144f706
2021BTech <daffydanni@gmail.com>
2022-10-24 02:51:58 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,3 +16,21 @@ void print_rev(char *s)
 
 	_putchar('\n');
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: returns lenght;
+ */
+int _strlen(char *s)
+{	int count, inc;
+
+	inc = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		inc++;
+	}
+
+	return (inc);
+}
