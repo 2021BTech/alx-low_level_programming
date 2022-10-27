5f6e766c88d11199bfb11f53ad2105b49e70a895
2021BTech <daffydanni@gmail.com>
2022-10-26 17:38:46 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,19 +6,37 @@
  */
 char *rot13(char *str)
 {
-	int i, j;
+	int index1, index2;
 
-	char input[80] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char output[80] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L',
+		 'M', 'N', 'O', 'P', 'Q', 'R',
+		  'S', 'T', 'U', 'V', 'W', 'X',
+		  'Y', 'Z', 'a', 'b', 'c', 'd',
+		  'e', 'f', 'g', 'h', 'i', 'j',
+		   'k', 'l', 'm', 'n', 'o', 'p',
+		   'q', 'r', 's', 't', 'u', 'v',
+		    'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y',
+		 'Z', 'A', 'B', 'C', 'D', 'E',
+		 'F', 'G', 'H', 'I', 'J', 'K',
+		 'L', 'M', 'n', 'o', 'p', 'q',
+		 'r', 's', 't', 'u', 'v', 'w',
+		  'x', 'y', 'z', 'a', 'b', 'c',
+		  'd', 'e', 'f', 'g', 'h', 'i',
+		  'j', 'k', 'l', 'm'};
 
-	for (i = 0; str[i] != '\0'; ++i)
+	while (str[++index1])
 	{
-		if (str[i] == input[j])
+		for (index2 = 0; index2 < 52; index2++)
 		{
-			str[i] = output[j];
-			break;
+			if (str[index1] == alphabet[index2])
+			{
+				str[index1] = rot13key[index2];
+				break;
+			}
 		}
 	}
 	return (str);
 }
-
